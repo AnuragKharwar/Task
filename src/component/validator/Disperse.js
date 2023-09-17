4878247f6e8cad3505b4fd9fde8ba8d39129c202
Anurag kharwar <88261615+AnuragKharwar@users.noreply.github.com>
2023-09-18 02:50:42 +0530

    adderess duplication check is done...recheking remain to do

--- src/component/validator/Disperse.js
+++ src/component/validator/Disperse.js
@@ -1,9 +1,22 @@
 import { useState } from "react";
+
 import "./Disperse.css";
 const Disperse = () => {
   const [Line, setLine] = useState([1]);
-  const [rawdata, setRawData] = useState([]);
   const [data, setData] = useState([]);
+  const [onlyAdd, setOnlyAdd] = useState([]);
+  const [duplicateAdd, setDuplicateAdd] = useState([]);
+  const wrongAmtIDx = [];
+
+  if (data) {
+    data.forEach((element, index) => {
+      if (!Number(+element.amount) && +element.amount !== 0) {
+        wrongAmtIDx.push(index + 1);
+      }
+    });
+  }
+
+  //   handling when key is pressed
   const handleKeyup = (e) => {
     handleInput(e);
     const numberofLine = e.target.value.split("\n").length;
@@ -14,34 +27,75 @@ const Disperse = () => {
   for (let i = 1; i <= Line && i <= 10; i++) {
     spanLine.push(<span key={i}></span>);
   }
+
   const handleInput = (e) => {
     if (e.key === "Enter") {
       const currInput = e.target.value.split("\n");
-      setRawData(currInput);
-    }
-  };
-  const handleSubmit = () => {
-    rawdata.map((item, idx) => {
-      const arr = item.split(/[\s,= ]+/);
-      const address = arr[0];
-      const amount = +arr[1];
-      console.log(amount);
+      const val = [];
 
-      setData([...data, { addrss: address, amt: 45 }]);
-    });
-    console.log(data);
+      currInput.forEach((item, idx) => {
+        if (idx !== currInput.length - 1 && item !== "") {
+          const arr = item.split(/[\s,= ]+/);
+
+          const address = arr[0];
+          const amount = arr[1];
+
+          if (onlyAdd.indexOf(address) >= 0) {
+            const previdx = onlyAdd.indexOf(address);
+
+            if (previdx !== idx && previdx !== -1)
+              if (
+                !duplicateAdd.find(
+                  (o, i) => o[0] === previdx + 1 && o[1] === idx + 1
+                )
+              )
+                setDuplicateAdd([...duplicateAdd, [previdx + 1, idx + 1]]);
+          }
+
+          setOnlyAdd([...onlyAdd, address]);
+          // console.log(address);
+
+          const temp = { address, amount };
+          val.push(temp);
+        }
+      });
+      setData(val);
+    }
   };
 
   return (
-    <div>
-      <h1>hello</h1>
+    <>
+      <div>
+        <h1>hello</h1>
 
-      <div className="editor">
-        <div className="line-numbers">{spanLine}</div>
-        <textarea onKeyUp={handleKeyup}></textarea>
+        <div className="editor">
+          <div className="line-numbers">{spanLine}</div>
+          <textarea onKeyUp={handleKeyup}></textarea>
+        </div>
       </div>
-      <button onClick={handleSubmit}>click me</button>
-    </div>
+      {wrongAmtIDx.length !== 0 ? (
+        <span className="error">
+          <p>Line {wrongAmtIDx[0]} wrong amount</p>
+        </span>
+      ) : (
+        ""
+      )}
+
+      {duplicateAdd.length !== 0 ? (
+        <p>
+          {duplicateAdd.map((item, idx) => {
+            return (
+              <li key={idx}>
+                Address {onlyAdd[item[0] - 1]} encounter duplicate in Line:{" "}
+                {item[0]},{item[1]}
+              </li>
+            );
+          })}
+        </p>
+      ) : (
+        ""
+      )}
+    </>
   );
 };
 
